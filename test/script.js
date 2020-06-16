4a07cdb6ef22e9245cc0a076b45c791b92638b67
Yuma Akado <yumaakado@Yuma-no-MacBook-Air.local>
2020-06-16 19:31:43 +0900

    chromeAPIテスト5

--- test/script.js
+++ test/script.js
@@ -1,17 +1,28 @@
-var URL = location.href;
-//'https://www.google.com/calendar/render?action=TEMPLATE&text=%E3%83%8A%E3%83%81%E3%83%A5%E3%83%A9%E3%83%AB%E3%83%AA%E3%83%83%E3%83%97%E3%82%92%E3%81%A4%E3%81%8F%E3%82%8BWorkshop&dates=20200628T040000Z/20200628T060000Z&location=%E6%9C%AA%E5%AE%9A&trp=true&trp=undefined&trp=true&';
-//本来はここで検索と分解
-/*var Calendar = 'g2466jvdp4rmvmbc23havk6dc4%40group.calendar.google.com';
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
 
-var NewURL = 'https://www.google.com/calendar/render?action=TEMPLATE' +
+var URL = document.querySelector("#groupEventShow > article > div > div.event-detail__description > dl:nth-child(1) > dd.event-detail__description--outside > a");
+var URL1 = URL.slice(0, 54);
+var result1 = URL.indexOf('&text');
+var result2 = URL.indexOf('&trp');
+var URL2 = URL.slice(result1, result2);
+var URL3 = URL.slice(result2);
+
+var Calendar = 'g2466jvdp4rmvmbc23havk6dc4%40group.calendar.google.com';
+var Info = 'テスト' //エンコードが必要
+var newURL = URL1 + '&src=' + Calendar + URL2 + '&details=' + Info + URL3;
+
+alert(newURL);
+
+/* var NewURL = 'https://www.google.com/calendar/render?action=TEMPLATE' +
 '&src=' + Calendar +
 '&text=%E7%AC%AC7%E5%9B%9E%20%E8%AA%AD%E6%9B%B8%E4%BC%9A%F0%9F%93%9A' +
 '&dates=20200628T010000Z/20200628T030000Z' +
-'&location=%E6%9C%AA%E5%AE%9A&trp=true&trp=undefined&trp=true&' +
+'&location=%E6%9C%AA%E5%AE%9A' +
 '&details=' + Info +
 '&trp=true&trp=undefined&trp=true&'; */
 
-chrome.tabs.create({url: URL}, function(tab));
+//chrome.tabs.create({url: URL}, function(tab));
 
 /*chrome.browserAction.onClicked.addListener(function() {
     chrome.tabs.create({ url: URL });
